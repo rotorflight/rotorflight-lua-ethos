749237632e6748bb32876e801226f2be9cdfc68a
egonl <egon.lubbers@gmail.com>
2024-07-02 20:29:53 +0200

    Optimized code

--- RF2/main.lua
+++ RF2/main.lua
@@ -36,8 +36,8 @@ local pageScrollY = 0
 local mainMenuScrollY = 0
 local telemetryState
 local PageFiles, Page, init, popupMenu
-local scrollTS = 0
-local speedMultiplier = 1
+local scrollSpeedTS = 0
+local scrollSpeedMultiplier = 1
 
 -- New variables for Ethos version
 local screenTitle = nil
@@ -457,10 +457,10 @@ local function wakeup(widget)
                 pageState = pageStatus.display
 				rf2.lcdNeedsInvalidate = true
             elseif lastEvent == EVT_VIRTUAL_NEXT then
-                incValue(1 * speedMultiplier)
+                incValue(1 * scrollSpeedMultiplier)
 				rf2.lcdNeedsInvalidate = true
             elseif lastEvent == EVT_VIRTUAL_PREV then
-                incValue(-1 * speedMultiplier)
+                incValue(-1 * scrollSpeedMultiplier)
 				rf2.lcdNeedsInvalidate = true
             end
         end
@@ -507,16 +507,16 @@ local function event(widget, category, value, x, y)
     --rf2.print("Event received: "..category.."  "..value)
     if category == EVT_KEY then
         if value == 4099 or value == 4100 then
-            scrollSpeed = rf2.clock() - scrollTS
+            local scrollSpeed = rf2.clock() - scrollSpeedTS
             --rf2.print(scrollSpeed)
             if scrollSpeed < 0.05 then
-                speedMultiplier = 10
+                scrollSpeedMultiplier = 10
             elseif scrollSpeed < 0.1 then
-                speedMultiplier = 5
+                scrollSpeedMultiplier = 5
             else
-                speedMultiplier = 1
+                scrollSpeedMultiplier = 1
             end
-            scrollTS = rf2.clock()
+            scrollSpeedTS = rf2.clock()
         end
 
         if value == EVT_VIRTUAL_PREV_LONG then
